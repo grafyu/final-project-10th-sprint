4d62f5b1128aafba644135eccbfb9ef01922d1ea
Yury, Yury Graf <grafy@mail.ru>
2024-05-13 00:10:56 +0300

    second variant

--- final10.sh
+++ final10.sh
@@ -12,13 +12,13 @@ mkdir task task/dir1 task/dir2 task/dir3 task/dir3/dir4
 cd ./task/
 
 # создаём пустой файл task/dir2/empty
-touch ./dir2/epmty
+touch ./dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
-echo '!#/bin/bash
-echo "$1, привет!"' > ./dir2/hello.sh
+echo '#!/bin/bash
+echo "$1 $2, привет!"' > ./dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
@@ -27,7 +27,7 @@ chmod 764 dir2/hello.sh
 ls ./dir2/ > ./dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp ./dir2/* ./dir3/dir4/
+cp dir2/* dir3/dir4/
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории (-printf "%f\n")
@@ -37,11 +37,11 @@ find . -type f -name "*.txt" > ./dir1/summary.txt
 cat ./dir2/list.txt >> ./dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME='Всем студентам'
+NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-echo "$NAME, привет!" >> ./dir1/summary.txt
+./dir2/hello.sh $NAME >> ./dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv ./dir1/summary.txt ./'Практическое задание'
